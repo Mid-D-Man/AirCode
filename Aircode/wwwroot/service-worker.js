be1864dedc9dcca717fe8110fcae15c704d9b12f
Abdulhamid Manman Suleiman <abdulhamid.m.321@gmail.com>
2025-02-28 15:44:40 +0100

    commit changes

--- Aircode/wwwroot/service-worker.js
+++ Aircode/wwwroot/service-worker.js
@@ -2,4 +2,4 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
-navigator.serviceWorker.register('./service-worker.js', {scope: './'}).then(r => console.log('Service worker registered', r)).catch(err => console.error('Service worker registration failed', err)); 
+ 
