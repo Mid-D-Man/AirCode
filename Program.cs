b59f9a93b62ff2262c730853ad11a82a4968d409
Abdulhamid Manman Suleiman <abdulhamid.m.321@gmail.com>
2025-03-13 18:32:54 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using AirCode;
+using AirCode.Services;
+using Blazored.LocalStorage;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//localstorage
+builder.Services.AddBlazoredLocalStorage();
+
+
+builder.Services.AddScoped<IScannerService, ScannerService>();
+
+//auth registry
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<IUserStorageService, UserStorageService>();
+
+await builder.Build().RunAsync();
+
+//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
