9596ab7ed029d4501f9df1477d47c82850c73d64
Abdulhamid Manman Suleiman <abdulhamid.m.321@gmail.com>
2025-06-05 20:15:51 +0100

    commi

--- AirCode/Program.cs
+++ AirCode/Program.cs
@@ -8,6 +8,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-//new reload
+//new 
 
 await builder.Build().RunAsync();
