c2accc8f14f2c3a97c82b4203e6f1f5160aa7a02
Abdulhamid Manman Suleiman <abdulhamid.m.321@gmail.com>
2025-06-21 12:59:16 +0100

    ok time to start offline,but first create actual ghost users and accounts to be used for propper testig

---
+++ AirCode/Pages/Admin/Shared/OfflineAttendanceEvent.razor.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components;
+
+namespace AirCode.Pages.Admin.Shared;
+
+public partial class OfflineAttendanceEvent : ComponentBase
+{
+}
---
+++ AirCode/Pages/Client/OfflineScanPage.razor.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components;
+
+namespace AirCode.Pages.Client;
+
+public partial class OfflineScanPage : ComponentBase
+{
+}
