293b5048ee8c60bd7dd07f79408ad19470a13db2
Abdulhamid Manman Suleiman <abdulhamid.m.321@gmail.com>
2025-06-21 01:19:34 +0100

    a

--- AirCode/Services/Attendance/IOfflineSyncService.cs
+++ AirCode/Services/Attendance/IOfflineSyncService.cs
@@ -3,6 +3,7 @@ using AirCode.Domain.Entities;
 namespace AirCode.Services.Attendance;
 public interface IOfflineSyncService
 {
+    //wai
     Task<bool> SyncPendingRecordsAsync();
     Task<SyncResult> ProcessOfflineAttendanceAsync(OfflineAttendanceRecord record);
     Task<SyncResult> SyncOfflineSessionAsync(OfflineSessionData session);
